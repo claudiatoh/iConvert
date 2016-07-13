d6fa816d5d3e156f45e6d2b3ab909f9d8b5ea36d
Claudia <claudiatohyian@hotmail.com>
2016-07-13 17:43:06 +0800

    Podfiles added.

--- iConvert/SecondViewController.m
+++ iConvert/SecondViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "SecondViewController.h"
+#import "CurrencyRequest/CRCurrencyRequest.h"
+#import "CurrencyRequest/CRCurrencyResults.h"
 
-@interface SecondViewController ()
+@interface SecondViewController ()<CRCurrencyRequestDelegate>
+@property (nonatomic) CRCurrencyRequest *req;
+@property (weak, nonatomic) IBOutlet UITextField *inputField;
+@property (weak, nonatomic) IBOutlet UIButton *convertButton;
+@property (weak, nonatomic) IBOutlet UILabel *currencyA;
+@property (weak, nonatomic) IBOutlet UILabel *currencyB;
+@property (weak, nonatomic) IBOutlet UILabel *currencyC;
 
 @end
 
@@ -23,5 +31,27 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)buttonPressed:(id)sender {
+    self.convertButton.enabled = NO;
+    self.req = [[CRCurrencyRequest alloc] init];
+    self.req.delegate = self;
+    [self.req start];
+}
+- (void)currencyRequest:(CRCurrencyRequest *)req
+    retrievedCurrencies:(CRCurrencyResults *)currencies{
+    self.convertButton.enabled = YES;
+    double inputValue = [self.inputField.text floatValue];
+    double eur = inputValue * currencies.EUR;
+    NSString *eurValue = [NSString stringWithFormat:@"%.2f", eur];
+    double jpy = inputValue * currencies.JPY;
+    NSString *jpyValue = [NSString stringWithFormat:@"%.2f", jpy];
+    double gbp = inputValue * currencies.GBP;
+    NSString *gbpValue = [NSString stringWithFormat:@"%.2f", gbp];
+    
+    self.currencyA.text = eurValue;
+    self.currencyB.text = jpyValue;
+    self.currencyC.text = gbpValue;
+}
+
 
 @end
